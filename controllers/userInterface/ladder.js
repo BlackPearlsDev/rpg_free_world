ddf234176cb0f4145917b9629c4d0c3ae5a153a6
BlackPearlsDev <proeslros2@gmail.com>
2022-09-01 14:37:15 +0200

    add limit to the ladder

--- controllers/userInterface/ladder.js
+++ controllers/userInterface/ladder.js
@@ -1,10 +1,10 @@
 import Main from "../../models/Main.js";
 
 export const pageLadder = async (req, res) => {
-    const queryDPS = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY dps DESC`; 
-    const queryPDV = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY health DESC`;
-    const queryArmor = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY armor DESC`;
-    const queryGold = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY gold DESC`;
+    const queryDPS = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY dps DESC LIMIT 10`; 
+    const queryPDV = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY health DESC LIMIT 10`;
+    const queryArmor = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY armor DESC LIMIT 10`;
+    const queryGold = `SELECT classeName, role, dps, health, armor, gold, accounts.id, accounts.pseudo FROM classes JOIN accounts ON classes.account_id = accounts.id ORDER BY gold DESC LIMIT 10`;
 
     const resultDPS = await Main.getData(queryDPS);
     const resultPDV = await Main.getData(queryPDV);
